8ca9d85d6c37fd8a671f5ea872372aceb4022367
Umar Khoirul Afif <110627263+Umar29072000@users.noreply.github.com>
2023-10-02 20:49:18 +0700

    Update_3

---
+++ resources/views/user/landingpage.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="{{ asset('assets/icons/logo-hmif.ico') }}">
+
+    <title>HMIF</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href={{ asset('assets/css/style.css') }}>
+</head>
+
+<body>
+
+    {{-- {{ navbar }} --}}
+    <nav class="navbar navbar-expand-lg text-black py-3 fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('assets/icons/logo-hmif.jpg') }}" height="55" width="55" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Prestasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Galery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Tentang Kami</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <button class="btn btn-danger">Register</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    {{-- {{ navbar }} --}}
+
+    <section id="hero">
+        <div class="container text-center text-black">
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
