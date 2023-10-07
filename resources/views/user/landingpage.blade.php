52ba04933a510cfcb9204b78441c34acc47c401c
Pid24 <rofidnasifannafie@gmail.com>
2023-10-07 17:48:11 +0700

    slicing hero section

--- resources/views/user/landingpage.blade.php
+++ resources/views/user/landingpage.blade.php
@@ -9,5 +9,6 @@
     <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
       <h1>HMIF - ITI</h1>
       <h5>Mengembangkan potensi mahasiswa Teknik Informatika dengan berlandaskan Tri Dharma Perguruan Tinggi</h5>
+      <h3>Periode 2023/2024</h3>
     </div>
   </section>
