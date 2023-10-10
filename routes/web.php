13ab7d8a6fc4fcbc5b5f504672d5c361a0c3c906
IlyasBudi <wjati145@gmail.com>
2023-10-10 12:39:11 +0700

    slicing prestasi page

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [LandingPageController::class, 'index']);
+
+Route::get('/prestasipage', function () {
+	return view('prestasipage');
+});
