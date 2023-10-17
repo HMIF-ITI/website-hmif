beb14354a621d0656b44e56fecbb1430dfc7c40c
reonaldi152 <reo@isysedge-soft.com>
2023-10-17 16:29:50 +0700

    change table name proker

--- database/migrations/2023_10_17_090911_create_proker_table.php
+++ database/migrations/2023_10_17_090911_create_proker_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('proker', function (Blueprint $table) {
+        Schema::create('prokers', function (Blueprint $table) {
             $table->id();
             $table->string('image');
             $table->string('title');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('proker');
+        Schema::dropIfExists('prokers');
     }
 };
