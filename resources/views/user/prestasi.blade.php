6489ac11e440d66c3856cd940dcfd716fee0ab76
IlyasBudi <wjati145@gmail.com>
2023-10-18 14:24:26 +0700

    slicing galeri

--- resources/views/user/prestasi.blade.php
+++ resources/views/user/prestasi.blade.php
@@ -10,15 +10,15 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
+  <title>Prestasi</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body>
 
-  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
-    <div class="container text-center text-md-left hero-text" data-aos="fade-up">
+  <section id="hero-prestasi" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="container text-center text-md-left hero-text-prestasi" data-aos="fade-up">
       <h1>Prestasi Mahasiswa <span>HMIF-ITI</span></h1>
       <h3>Berkilau di Puncak Prestasi! Halaman Prestasi Mahasiswa</h3>
       <h3>HMIF-ITI, Tempat Inspirasi!</>
@@ -30,7 +30,7 @@
     <div class="row row-cols-lg-3 row-cols-md-2 row-cols-1 g-4 p-5">
       <div class="col">
         <div class="card prestasi-card">
-          <div class="card-body p-0"><img src="{{ asset('assets/image/card-thumbnail.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
+          <div class="card-body p-0"><img src="{{ asset('assets/image/card-prestasi-img.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
             <div class="p-4">
               <h5 class="mb-0 card-title">Mahasiswa ITI Prodi Teknik Informatika berhasil memenangkan lomba web design</h5>
               <a href="#" class="small">baca selengkapnya ></a>
@@ -40,7 +40,7 @@
       </div>
       <div class="col">
         <div class="card rounded-2 prestasi-card">
-          <div class="card-body p-0"><img src="{{ asset('assets/image/card-thumbnail.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
+          <div class="card-body p-0"><img src="{{ asset('assets/image/card-prestasi-img.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
             <div class="p-4">
               <h5 class="mb-0 card-title">Mahasiswa ITI Prodi Teknik Informatika berhasil memenangkan lomba web design</h5>
               <a href="#" class="small">baca selengkapnya ></a>
@@ -50,7 +50,7 @@
       </div>
       <div class="col">
         <div class="card rounded-2 prestasi-card">
-          <div class="card-body p-0"><img src="{{ asset('assets/image/card-thumbnail.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
+          <div class="card-body p-0"><img src="{{ asset('assets/image/card-prestasi-img.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
             <div class="p-4">
               <h5 class="mb-0 card-title">Mahasiswa ITI Prodi Teknik Informatika berhasil memenangkan lomba web design</h5>
               <a href="#" class="small">baca selengkapnya ></a>
@@ -60,7 +60,7 @@
       </div>
       <div class="col">
         <div class="card rounded-2 prestasi-card">
-          <div class="card-body p-0"><img src="{{ asset('assets/image/card-thumbnail.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
+          <div class="card-body p-0"><img src="{{ asset('assets/image/card-prestasi-img.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
             <div class="p-4">
               <h5 class="mb-0 card-title">Mahasiswa ITI Prodi Teknik Informatika berhasil memenangkan lomba web design</h5>
               <a href="#" class="small">baca selengkapnya ></a>
@@ -70,7 +70,7 @@
       </div>
       <div class="col">
         <div class="card rounded-2 prestasi-card">
-          <div class="card-body p-0"><img src="{{ asset('assets/image/card-thumbnail.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
+          <div class="card-body p-0"><img src="{{ asset('assets/image/card-prestasi-img.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
             <div class="p-4">
               <h5 class="mb-0 card-title">Mahasiswa ITI Prodi Teknik Informatika berhasil memenangkan lomba web design</h5>
               <a href="#" class="small">baca selengkapnya ></a>
@@ -80,7 +80,7 @@
       </div>
       <div class="col">
         <div class="card rounded-2 prestasi-card">
-          <div class="card-body p-0"><img src="{{ asset('assets/image/card-thumbnail.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
+          <div class="card-body p-0"><img src="{{ asset('assets/image/card-prestasi-img.svg') }}" alt="" class="w-100 card-img-top prestasi-img">
             <div class="p-4">
               <h5 class="mb-0 card-title">Mahasiswa ITI Prodi Teknik Informatika berhasil memenangkan lomba web design</h5>
               <a href="#" class="small">baca selengkapnya ></a>
