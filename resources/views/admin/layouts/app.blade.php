bad9e4bc2193aa06f27c4c0be59ec773aac83510
Umar Khoirul Afif <110627263+Umar29072000@users.noreply.github.com>
2023-10-21 22:03:56 +0700

    ada yang ketinggalan kwkwkw
    ada yang ketinggalan kwkwkw

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -36,7 +36,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="/dashboard" class="brand-link">
+            <a href="/admin/dashboard" class="brand-link">
                 <img src="{{ asset('assets-admin/dist/img/hmif.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">Admin HMIF</span>
